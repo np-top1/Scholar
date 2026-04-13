ea13746227c4e6e6867c217e208e1ad697a619b7
NP <nathanpardo2011@gmail.com>
2026-04-13 18:51:05 -0400

    feat: Migrate to react-ga4 for analytics
    Upgrades the Google Analytics integration from react-ga to react-ga4 to ensure compatibility with GA4 properties and leverage updated tracking methods.

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useEffect } from 'react';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 const Analytics = () => {
   useEffect(() => {
     ReactGA.initialize('G-Y74VS67LX3');
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
   }, []);
 
   return null;
